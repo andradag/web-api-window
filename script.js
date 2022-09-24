956894f3db7b337b836194054ee32c6f64895c3f
Andrada Gacichevici <andradagacichevici@gmail.com>
2022-09-24 10:29:40 +0100

    html and js

---
+++ script.js
@@ -0,0 +1,23 @@
+// Logs window object
+console.log(window);
+
+// Logs reference to the document in the window object
+console.log(window.document);
+
+// Logs root element of document.
+console.log(document.documentElement);
+
+// Returns head element of current document
+console.log(document.head);
+
+// Logs window object using this
+console.log("this: ");
+console.log(this);
+
+// Logs the document object
+console.log("window.document: ");
+console.log(window.document);
+
+// Logs body of document
+console.log("document.body: ");
+console.log(document.body.children[0].textContent);
